2efc610c30c888a2924c15fc203da6f33e4bc4d8
ngwq <ngwq2006@mail.ru>
2025-07-03 19:06:12 +0400

    feat: add sum of integers

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 
-int main() { std::cout << "Aboba v1.1" << std::endl; }
+int main() {
+    std::cout << "Aboba v1.1" << std::endl;
+    int a = 907;
+    int b = 279;
+    int c = a + b;
+    std::cout << c << std::endl;
+}
