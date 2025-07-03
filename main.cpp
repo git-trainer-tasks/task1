39cd749dd49fdbdc0dcbd9fe179915fa974b7d0f
ngwq <ngwq2006@mail.ru>
2025-07-03 19:10:00 +0400

    feat: add integer division

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ int main() {
     std::cout << "Aboba v1.1" << std::endl;
     int a = 907;
     int b = 279;
-    int c = a + b;
-    std::cout << c << std::endl;
+    int c = 11037;
+    int d = 87;
+    int e = a + b + c / d;
+    std::cout << e << std::endl;
 }
