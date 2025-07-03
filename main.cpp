8ef6c23e61648728ad5d070147f65bb07bb66451
ngwq <ngwq2006@mail.ru>
2025-07-03 19:13:29 +0400

    feat: new version 1.3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Aboba v1.1" << std::endl;
+    std::cout << "Aboba v1.3" << std::endl;
     int a = 907;
     int b = 279;
     int c = 11037;
-    int d = 87;
-    int e = a + b + c / d;
-    std::cout << e << std::endl;
+    int d = 0;
+    int e = 116;
+    int f = a + b + c / d + e * a;
+    std::cout << f << std::endl;
 }
